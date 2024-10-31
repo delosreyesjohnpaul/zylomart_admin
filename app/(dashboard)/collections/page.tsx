bfa63bcbeb4965c427f19ef41c4e6dab1d7f7c0f
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-31 15:26:00 +0800

    deletefunction

--- app/(dashboard)/collections/page.tsx
+++ app/(dashboard)/collections/page.tsx
@@ -1,34 +1,51 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { Plus } from "lucide-react";
 
-const CollectionsPage = () => {
-    const [loading, setLoading] = useState(true);
-    const [collections, setCollections] = useState([]);
+import { columns } from "@/components/collections/CollectionColumns";
+import { DataTable } from "@/components/custom ui/DataTable";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import Loader from "@/components/custom ui/Loader";
 
-    const getCollections = async () => {
-        try {
-          const res = await fetch("/api/collections", {
-            method: "GET",
-          });
-          const data = await res.json();
-          setCollections(data);
-          setLoading(false);
-        } catch (err) {
-          console.log("[collections_GET]", err);
-        }
-    };
+const Collections = () => {
+  const router = useRouter();
 
-    useEffect(() => {
-        getCollections();
-    }, []);
+  const [loading, setLoading] = useState(true);
+  const [collections, setCollections] = useState([]);
 
-    console.log(collections)
-    
-    
-    return (
-        <div>CollectionsPage</div>
-    )
-}
+  const getCollections = async () => {
+    try {
+      const res = await fetch("/api/collections", {
+        method: "GET",
+      });
+      const data = await res.json();
+      setCollections(data);
+      setLoading(false);
+    } catch (err) {
+      console.log("[collections_GET]", err);
+    }
+  };
 
-export default CollectionsPage;
+  useEffect(() => {
+    getCollections();
+  }, []);
+
+  return loading ? <Loader /> : (
+    <div className="px-10 py-5">
+      <div className="flex items-center justify-between">
+        <p className="text-heading2-bold">Collections</p>
+        <Button className="bg-blue-1 text-white" onClick={() => router.push("/collections/new")}>
+          <Plus className="h-4 w-4 mr-2" />
+          Create Collection
+        </Button>
+      </div>
+      <Separator className="bg-grey-1 my-4" />
+      <DataTable columns={columns} data={collections} searchKey="title" />
+    </div>
+  );
+};
+
+export default Collections;
